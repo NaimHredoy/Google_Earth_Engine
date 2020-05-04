4b20be86be92b2af5fcfbe2d93770d8c0840d66c
MD SHAH NAIM HREDOY <34861566+NaimHredoy@users.noreply.github.com>
2020-05-04 16:35:29 +0600

    Update Importing and Filtering data.java

--- Importing and Filtering data.java
+++ Importing and Filtering data.java
@@ -1,8 +1,19 @@
-var aoi= l8.filterBounds(area)
+//Md Shah Naim Hredoy
+//hridhowlader@gmail.com
+
+//after imporint data e.g., Landsat or MODIS etc. select the area of interest as aoi and declaring it as a variable
+//filtering by methadata and date
+
+var aoi= l8.filterBounds(area)        
         .filterMetadata("CLOUD_COVER", "less_than", 3)
         .filterDate("2019-06-01","2020-01-01");
-        
+
+//pring all images and their size
 print(aoi);
 print (aoi.size());
+
+//select the median pixel value from all images
 var median= aoi.median();
+
+//map visualization
 Map.addLayer(median, vision,"My map",true, 1)
